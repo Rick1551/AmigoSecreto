54fc1bbfa6f5f91c8d420ca167bc8325dae29ca5
Ricardo Saldaña <ricardo_scuentas@outlook.com>
2025-08-06 17:37:40 -0500

    Version final

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    const inputNombre = document.getElementById('amigo'); 
+
+    if (inputNombre.value === '') {
+        alert('El campo no puede estar vacío');
+        return;
+    }
+
+    // Agregar nombre a la lista
+    const nuevoElemento = document.createElement('li');
+    nuevoElemento.textContent = inputNombre.value;
+    amigos.push(inputNombre.value);
+    listaAmigos.appendChild(nuevoElemento);
+
+    // Limpiar campo de texto
+    inputNombre.value = '';
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('No hay amigos para sortear');
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+    document.getElementById('resultado').textContent = amigoSorteado;
+
+    //limpiar la lista y el arreglo
+    amigos = [];
+    listaAmigos.innerHTML = '';
+}
